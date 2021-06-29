bc6596cb80094bf16d2debd37b637443282b2ee1
Abu Sakib <mabusakib@gmail.com>
2021-06-29 21:24:11 +0600

    Refactor so that it looks less horrible... see body for more details
    * Refactor `choose_process` to follow more idiomatic pattern. Now it truly lives up to its name!
    * Add a new function `print_matches`, that prints the matching lines, with/without line numbers.
    * Add documentation
    * If I missed anything my bad. I'm still recovering from my illness and I thought it'd be a healing drug to make the commit now...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+//! Searches for patterns. Prints to the standard output after successful match.
 use clap::{App, Arg};
 use colored::Colorize;
 use regex::Regex;
@@ -6,12 +7,21 @@ use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+/// Struct representting the argument flags.
+///
+/// # Respective flags represented by the fields:
+/// ```
+/// --count, -c
+/// --line-number, -n
+/// --color
+/// ```
 struct Flags {
     count: bool,
     line_number: bool,
     colorize: bool,
 }
 
+/// Calculates the number of matches found according to the regex pattern and returns it
 fn count_matches<T: BufRead + Sized>(reader: T, re: Regex) -> u32 {
     let mut matches: u32 = 0;
     for line_ in reader.lines() {
@@ -24,58 +34,50 @@ fn count_matches<T: BufRead + Sized>(reader: T, re: Regex) -> u32 {
     matches
 }
 
+/// Checks status of the `count` field in Flags struct
+/// If it's set, then display number of matches returned by calling `count_matches`
+/// Otherwise, call `print_matches`
+fn choose_process<T: BufRead + Sized>(reader: T, re: Regex, flags: &Flags) {
+    match flags.count {
+        // supress normal output and only print total number of matches found
+        true => println!("{}", count_matches(reader, re)),
+        // call `print_matches` to print matching lines
+        false => print_matches(reader, re, flags),
+    }
+}
+
+/// Uses the `colored` crate to colorize the passed pattern
 fn colorize_pattern(pattern: &str) -> String {
     pattern.red().to_string()
 }
 
-// This function is horrible. Just horrible code...
-fn choose_process<T: BufRead + Sized>(reader: T, re: Regex, flags: &Flags) {
-    if flags.count {
-        println!("{}", count_matches(reader, re));
-    } else {
-        if flags.line_number && !flags.colorize {
-            for (i, line_) in reader.lines().enumerate() {
-                let line = line_.unwrap();
-                match re.find(&line) {
-                    Some(_) => println!("{}: {}", i + 1, line),
-                    None => (),
-                }
-            }
-        } else if flags.line_number && flags.colorize {
-            for (i, line_) in reader.lines().enumerate() {
-                let line = line_.unwrap();
-                let pattern = match re.find(&line) {
-                    Some(m) => m.as_str(),
-                    None => "none",
-                };
-                if pattern != "none" {
-                    let colorized_pattern = colorize_pattern(&pattern);
-                    println!("{}: {}", i + 1, re.replace_all(&line, colorized_pattern));
-                }
-            }
-        } else if !flags.line_number && flags.colorize {
-            for line_ in reader.lines() {
-                let line = line_.unwrap();
-                let pattern = match re.find(&line) {
-                    Some(m) => m.as_str(),
-                    None => "none",
-                };
-                if pattern != "none" {
-                    let colorized_pattern = colorize_pattern(&pattern);
-                    println!("{}", re.replace_all(&line, colorized_pattern));
-                }
-            }
-        } else {
-            for line_ in reader.lines() {
-                let line = line_.unwrap();
-                match re.find(&line) {
-                    Some(_) => println!("{}", line),
-                    None => (),
-                }
-            }
+/// Prints the lines containing the matches found.
+/// Based on the status of the `line_number` field of Flag struct, 
+/// also prints the 1-based line number preceeding each line. 
+fn print_matches<T: BufRead + Sized>(reader: T, re: Regex, flags: &Flags) {
+    // `.lines()` returns an iterator over each line of `reader`, in the form of `io::Result::String`
+    // So a line would be an instance like this: `Ok(line)`
+    // `enumerate` gives us the (index, value) pair
+    let mut lines = reader.lines().enumerate();
+
+    // `.next()` on an iterator returns the item wrapped in an Option
+    // So Each `Some` variant of that option will hold the (index, value) pair
+    while let Some((i, Ok(line))) = lines.next() {
+        let pattern = match re.find(&line) {
+            // `re.find()` returns the byte range holding the first match
+            // as_str() returns that match in text form 
+            Some(pattern) => pattern.as_str(),
+            None => continue,
+        };
+        match (flags.line_number, flags.colorize) {
+            (true, false) => println!("{}: {}", i + 1, line),
+            (false, true) => println!("{}", re.replace_all(&line, colorize_pattern(pattern))),
+            (true, true) => println!("{}: {}", i + 1, re.replace_all(&line, colorize_pattern(pattern))),
+            _ => println!("{}", line),
         }
     }
 }
+
 fn main() {
     let args = App::new("grab")
         .version("0.1")
@@ -118,7 +120,7 @@ fn main() {
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
-    let input = args.value_of("input").unwrap_or("-");
+    let input = args.value_of("input").unwrap_or("STDIN");
 
     let count_flag = args.is_present("count");
     let line_number_flag = args.is_present("line numbers");
@@ -132,7 +134,8 @@ fn main() {
 
     let re = Regex::new(pattern).unwrap();
 
-    if input == "-" {
+    // if `input` argument was not given then take input from STDIN
+    if input == "STDIN" {
         let stdin = io::stdin();
         let reader = stdin.lock();
         choose_process(reader, re, &flags);
